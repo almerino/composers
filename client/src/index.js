2747e6a0132003efef0dab9a36b2515cc80caed7
almerino <almerino22@gmail.com>
2017-07-10 13:38:44 +0200

    prepare graphl client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT })
+
+export const client = new ApolloClient({ networkInterface })
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
